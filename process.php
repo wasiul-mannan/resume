5644bdbd855d18e548f7fa78ccfb09033db39437
wasiul-mannan <amd.wasiulmannan@gmail.com>
2023-04-12 13:08:36 +0600

    a

--- process.php
+++ process.php
@@ -1,24 +1,27 @@
 <?php
+// Retrieve the form data
+$name = $_POST['name'];
+$email = $_POST['email'];
+$phone = $_POST['phone'];
+$message = $_POST['message'];
 
-    $to = "mr3essa@gmail.com";
-    $from = $_REQUEST['email'];
-    $name = $_REQUEST['name'];
-    $headers = "From: $from";
-    $subject = "You have a message.";
+// Validate and sanitize the form data
+$name = filter_var($name, FILTER_SANITIZE_STRING);
+$email = filter_var($email, FILTER_SANITIZE_EMAIL);
+$phone = filter_var($phone, FILTER_SANITIZE_STRING);
+$message = htmlspecialchars($message);
 
-    $fields = array();
-    $fields{"name"} = "name";
-    $fields{"email"} = "email";
-    $fields{"phone"} = "phone";
-    $fields{"message"} = "message";
+// Construct the email message
+$to = "wasiul@atmotechisf.co";
+$subject = "New Contact Message";
+$body = "Name: $name\nEmail: $email\nPhone: $phone\nMessage: $message";
 
-    
-
-    $body = "Here is what was sent:\r\n"; 
-
-    foreach($fields as $a => $b){$body .= $b." : ".$_REQUEST[$a]."\r\n"; }
-
-
-    $send = mail($to, $subject, $body, $headers);
-
+// Send the email
+if (mail($to, $subject, $body)) {
+    // Email sent successfully
+    header("Location: contact.php?status=success");
+} else {
+    // Email failed to send
+    header("Location: contact.php?status=error");
+}
 ?>
